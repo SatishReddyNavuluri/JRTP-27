8f03cac7f2c964907623f27bef4332ba67f17271
SatishReddyNavuluri <satishreddynavuluri2@gmail.com>
2025-04-08 11:50:26 +0100

    task m1() completed

--- hello.java
+++ hello.java
@@ -1,9 +1,18 @@
-class Hello{
+class Hello
+{
 	public statuc void main(String[] args)
 	{
+		public void m1(){
+			//logic
+			System.out.println("m1-A");
+
+
+		}
+
 		public void m2()
 		{
 			System.out.prinltn("m2-hello");
 		}
+		
 	}
 }
